8293638b9e4e4e3220f072a752a712975491f447
unknown <afiqf1200@gmail.com>
2022-06-18 23:58:11 +0700

    ketiga

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-	
+from googletrans import Translator
+translator = Translator(service_urls=['translate.googleapis.com'])
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def main():
-    return "Welcome!"
+    kata = "Saya"
+    data = translator.translate(kata, dest='en')
+    t = data.text
+    return(t)
 if __name__ == "__main__":
     app.run()
