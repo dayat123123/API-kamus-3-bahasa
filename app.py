5e49d4b425263831dea77f710230934854125303
unknown <dayatburgerkill389@gmail.com>
2022-06-19 21:01:44 +0700

    ada

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import json
+from urllib import response
 from googletrans import Translator
 translator = Translator(service_urls=['translate.googleapis.com'])
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 @app.route("/")
 def main():
@@ -8,5 +10,22 @@ def main():
     data = translator.translate(kata, dest='en')
     t = data.text
     return(t)
+@app.route('/name', methods = ['GET', 'POST'])
+def nameRoute():
+
+    #fetching the global response variable to manipulate inside the function
+    global response
+    #checking the request type we get from the app
+    if(request.method == 'POST'):
+        request_data = request.data #getting the response data
+        request_data = json.loads(request_data.decode('utf-8')) #converting it from json to key value pair
+        name = request_data['name'] #assigning it to name
+        response1 = f'Hi! this is Python' #re-assigning response with the name we got from the user
+        response = translator.translate(response1, dest='id')
+        return " " #to avoid a type error
+    else:
+        return jsonify({'name' : response}) #sending data back to your frontend app
+
 if __name__ == "__main__":
     app.run()
+
