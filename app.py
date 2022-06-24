c4e4ada26fb3ea32d091dabec2e328cc941d5873
dayat123123 <dayatburgerkill389@gmail.com>
2022-06-24 19:31:56 +0700

    saya

--- app.py
+++ app.py
@@ -13,23 +13,20 @@ app = Flask(__name__)
 @app.route('/name', methods = ['GET', 'POST'])
 def nameRoute():
 
-    #fetching the global response variable to manipulate inside the function
     global response
     global adda
     global adc
     global hasil
     #checking the request type we get from the app
-    if(request.method == 'POST'):
-        request_data = request.data #getting the response data
-        request_data = json.loads(request_data.decode('utf-8')) #converting it from json to key value pair
-        name = request_data['name'] #assigning it to name
-        adda = f'{name}' #re-assigning response with the name we got from the user
-        adc =  translator.translate(adda, dest='en')
-        hasil = adc.text
-        response = hasil
-        return jsonify({'name' : response})
-    else:
-        return jsonify({'name' : response}) #sending data back to your frontend app
+
+    request_data = request.data #getting the response data
+    request_data = json.loads(request_data.decode('utf-8')) #converting it from json to key value pair
+    name = request_data['name'] #assigning it to name
+    adda = f'{name}' #re-assigning response with the name we got from the user
+    adc =  translator.translate(adda, dest='en')
+    hasil = adc.text
+    response = hasil
+    return jsonify({'name' : response}) #sending data back to your frontend app
 @app.route('/name2', methods = ['GET', 'POST'])
 def nameRoute2():
 
