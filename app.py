f8e9c21ee43cf484434e2f2de7b27fcc0d8fb93b
dayat123123 <dayatburgerkill389@gmail.com>
2022-06-28 14:24:28 +0700

    Tambah lgi

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def returnvalue3():
     d['output'] = answer
     return d
 @app.route('/api4', methods = ['GET'])
-def returnvalue3():
+def returnvalue4():
     global banjarindo
     d = {}
     inputchr = str(request.args['query'])
@@ -95,7 +95,8 @@ def returnvalue3():
     cur.execute("SELECT kata_dasar FROM tb_katadasar2 where kata_daerah = %s", [inputchr])
     mysql.connection.commit()
     banjarindo=cur.fetchall()
-    d['output'] = banjarindo
+    answer = banjarindo
+    d['output'] = answer
     return jsonify(d)
 if __name__ == "__main__":
     app.run()
